a3af8a6717f708e722583266443d992e83aa4ddf
KerryPower <kerrycpower@gmail.com>
2024-06-08 10:17:30 +1200

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+
+    const csvToJson = (csv) => {
+        // Students should implement this
+        const personalDetails = csv.split("\n");
+    
+    };
+    const csv = "id,name,email,phone\n1,John Doe,john@example.com,555-555-5555\n2,Jane Doe,jane@example.com,555-555-5556";
+    const customers = [
+        { id: 1, name: 'John Doe', email: 'john@example.com', phone: '555-555-5555' },
+        { id: 2, name: 'Jane Doe', email: 'jane@example.com', phone: '555-555-5556' }
+    ]
+    console.log(personalDetails);
